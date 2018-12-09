c014c2c697a299a3309d607fe2289408c96d8cd1
Pavel <pavel164rus1@gmail.com>
2018-12-09 21:32:22 +0400

    d

--- MethodRungeKutta/src/main/java/Main.java
+++ MethodRungeKutta/src/main/java/Main.java
@@ -7,45 +7,42 @@ public class Main {
     public static void main(String args[]) {
         Differential d2 = new Differential();
         double x0 = 0, y = 0, x = 0, h = 0.1;
-        for (int i = 0; i < 10; i++) {
-            System.out.println("in " + x + " value of y at x is : "
-                    + d2.rungeKutta(x0, y, x, h));
-            x += 0.1;
+
+
+        double[][] result = new double[2][11];
+        double[] q = new double[11];
+        double[] dq = new double[11];
+        double[] dq2 = new double[11];
+
+        int xd = 0;
+        for (double i = 0; i < 0.3; xd++, i = i + 0.1) {
+            result[0][xd] = i;
+            result[1][xd] = d2.rungeKutta(x0, y, i, h);
         }
-
-
-        x0 = 0;
-        y = 0;
-        x = 0;
-        h = 0.1;
-
-        double[][] result = new double[2][10];
-        double[] q = new double[10];
-        double[] q1 = new double[10];
-        double[] dq2 = new double[10];
-
-
-        for (double i = 0, xd=0; i < 0.3;xd++, i=i+0.1) {
-            result[0][(int)xd] = i;
-            result[1][(int)xd] = d2.rungeKutta(x0, y, i, h);
-        }
-
-        q[0] = 0.1 * d2.dydx(0,0);
-        q1[0] = 0.1 * d2.dydx(0,0);
-        q2[0] = 0.1 * d2.dydx(0,0);
 
         for (int i = 0; i < 3; i++) {
-
+            q[i] = 0.1 * d2.dydx(result[0][i], result[1][i]);
+            q[i + 1] = 0.1 * d2.dydx(result[0][i + 1], result[1][i + 1]);
+            dq[i] = q[i + 1] - q[i];
+            dq2[i] = dq[i + 1] - dq[i];
         }
 
-        for (int i = 3; i < result.length; i++) {
+        xd = 3;
+        for (double i = 0.3; i < 1; xd++, i = i + 0.1) {
+            result[0][xd] = i;
+            result[1][xd] = result[1][xd - 1] + q[xd - 1] + 0.5 * dq[xd - 1] + (5 / 12) * dq2[xd - 2];
 
+
+            q[xd] = 0.1 * d2.dydx(result[0][xd], result[1][xd]);
+            dq[xd - 1] = q[xd] - q[xd - 1];
+            dq2[xd - 1] = dq[xd] - dq[xd - 1];
         }
 
 
-        System.out.println("\t i \t x \ty \tq0 \tq1 \tdq2");
-        for (int i = 0; i < 10; i++) {
-            System.out.print("\n\t" + i+ "\t" + result[0][i] + " \t "+ result[1][i] + "\t1  \t1 \t1 ");
+        for (int i = 0; i < 11; i++) {
+
+            if (i == 0) System.out.println("\ti\tx  \ty    \tq0     \tq1     \tdq2");
+            System.out.println("\t" + i + "\t" + (Math.ceil(result[0][i] * 10)) / 10.0 + "\t " + ((int) (result[1][i] * 1000)) / 1000.0 + "\t" + ((int) (q[i] * 1000)) / 1000.0 + "\t" + ((int) (dq[i] * 10000)) / 10000.0 + "\t" + ((int) (dq2[i] * 10000)) / 10000.0);
 
         }
 
